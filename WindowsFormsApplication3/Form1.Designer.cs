80d8d36840b98e1db04f894ffb63d65c8476b68d
Gabe Newell Baruga <vovancik99vovan@gmai.com>
2015-02-14 15:48:47 +0300

    Добавил проект

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Classical = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Magical = new System.Windows.Forms.ListBox();
+            this.Rare = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(497, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(479, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Аттака";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(468, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 151);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(140, 151);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(233, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(468, 227);
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Value = 1000;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.progressBar2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.progressBar2.Location = new System.Drawing.Point(22, 227);
+            this.progressBar2.Maximum = 1000;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 7;
+            this.progressBar2.Value = 1000;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(52, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "7";
+            // 
+            // Classical
+            // 
+            this.Classical.FormattingEnabled = true;
+            this.Classical.Items.AddRange(new object[] {
+            "Кинжал",
+            "Меч",
+            "Пила",
+            "Топор",
+            "БензоПила",
+            "Палка",
+            "Гитара",
+            "Нож",
+            "Вилка",
+            "Клавиатура",
+            "Нунчаки",
+            "Шведский Нож"});
+            this.Classical.Location = new System.Drawing.Point(-3, 256);
+            this.Classical.Name = "Classical";
+            this.Classical.Size = new System.Drawing.Size(84, 95);
+            this.Classical.TabIndex = 9;
+            this.Classical.Visible = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(169, 227);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(107, 95);
+            this.listBox2.TabIndex = 10;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "\r\n";
+            this.notifyIcon1.Visible = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(282, 227);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(39, 95);
+            this.listBox3.TabIndex = 11;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(327, 227);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(39, 95);
+            this.listBox4.TabIndex = 13;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(373, 228);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(39, 95);
+            this.listBox5.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Цена";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(333, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Урон";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(375, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 26);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Шанс\r\nКрита\r\n";
+            // 
+            // Magical
+            // 
+            this.Magical.FormattingEnabled = true;
+            this.Magical.Items.AddRange(new object[] {
+            "Magic || Кинжал ",
+            "Magic || Меч",
+            "Magic || Пила",
+            "Magic || Топор",
+            "Magic || БензоПила",
+            "Magic || Палка",
+            "Magic || Гитара",
+            "Magic || Нож",
+            "Magic || Вилка",
+            "Magic || Клавиатура",
+            "Magic || Нунчаки",
+            "Magic || Шведский Нож"});
+            this.Magical.Location = new System.Drawing.Point(58, 256);
+            this.Magical.Name = "Magical";
+            this.Magical.Size = new System.Drawing.Size(84, 95);
+            this.Magical.TabIndex = 19;
+            this.Magical.Visible = false;
+            // 
+            // Rare
+            // 
+            this.Rare.FormattingEnabled = true;
+            this.Rare.Items.AddRange(new object[] {
+            "Rare || Кинжал ",
+            "Rare || Меч",
+            "Rare || Пила",
+            "Rare || Топор",
+            "Rare || БензоПила",
+            "Rare || Палка",
+            "Rare || Гитара",
+            "Rare || Нож",
+            "Rare || Вилка",
+            "Rare || Клавиатура",
+            "Rare || Нунчаки",
+            "Rare || Шведский Нож"});
+            this.Rare.Location = new System.Drawing.Point(122, 256);
+            this.Rare.Name = "Rare";
+            this.Rare.Size = new System.Drawing.Size(84, 95);
+            this.Rare.TabIndex = 20;
+            this.Rare.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(625, 356);
+            this.Controls.Add(this.Rare);
+            this.Controls.Add(this.Magical);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.Classical);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Classical;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox Magical;
+        private System.Windows.Forms.ListBox Rare;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        int BossLife = 1000, MaxBossLife = 1000, Hp = 1000, damage, BossDamage;
+        Random rnq = new Random();    
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Random rn = new Random();
+            
+            damage = 10 + rn.Next(-5, +5);
+            label1.ForeColor = Color.Lime;
+            if (rn.Next(0, 100) <= 25)
+            {
+                damage = 50 + rn.Next(-5, +5);
+                label1.ForeColor = Color.Red;
+            }
+
+            BossDamage = 7 + rn.Next(-5, +5);
+            label2.ForeColor = Color.Lime;
+            if (rn.Next(0, 100) <= 15)
+            {
+                BossDamage = 60 + rn.Next(-5, +5);
+                label2.ForeColor = Color.Red;
+            }
+            
+            Hp -= BossDamage;
+            if (progressBar2.Value - damage <= 0)
+            {
+                MaxBossLife += 250;
+                BossLife = MaxBossLife;
+                progressBar2.Maximum = BossLife;
+                progressBar2.Value = progressBar2.Maximum;
+                //pictureBox3.Visible = false;
+                if (rnq.Next(0, 100) <= 5)
+                {
+                    listBox2.Items.Add(Rare.Items[rnq.Next(0, Rare.Items.Count)]);
+                    listBox3.Items.Add(rnq.Next(5000, 50000));
+                    listBox4.Items.Add(rnq.Next(500, 5000));
+                    listBox5.Items.Add(rnq.Next(25, 99));
+                }
+                else
+                {
+                    if (rnq.Next(0, 100) <= 35)
+                    {
+                        listBox2.Items.Add(Magical.Items[rnq.Next(0, Magical.Items.Count)]);
+                        listBox3.Items.Add(rnq.Next(500, 15000));
+                        listBox4.Items.Add(rnq.Next(500, 1500));
+                        listBox5.Items.Add(rnq.Next(25, 70));
+                    }
+                    else
+                    {
+                        listBox2.Items.Add(Classical.Items[rnq.Next(0, Classical.Items.Count)]);
+                        listBox3.Items.Add(rnq.Next(1, 500));
+                        listBox4.Items.Add(rnq.Next(1, 500));
+                        listBox5.Items.Add(rnq.Next(1, 50));
+                    }
+                }
+                
+                
+                Hp = 1000;
+                progressBar1.Value = Hp;
+            }
+            else
+            {
+                progressBar2.Value -= damage;
+                label2.Text = BossDamage.ToString();
+            }
+            if (progressBar1.Value - BossDamage <= 0)
+            {
+                Form2 f = new Form2();
+                f.Show();
+
+            }
+            else
+            {
+                progressBar1.Value -= BossDamage;
+                label1.Text = damage.ToString();
+                
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        }
+    }
+
